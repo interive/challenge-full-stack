fc668dcaf127f42142ce4956756c9194b2086563
interive <sLx8IFfpu3UvdjsOi%iQ82>
2020-07-17 16:53:14 +0100

    Docker: refactor dockerfiles, add docker-compose.yml

---
+++ api/src/main/java/doodle/fullstack/SpringBootChat.java
@@ -0,0 +1,13 @@
+package doodle.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChat {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChat.class, args);
+	}
+
+}
