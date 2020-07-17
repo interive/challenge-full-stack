966b3c629f61cdb214ecf06c96870d85520dbbd7
interive <sLx8IFfpu3UvdjsOi%iQ82>
2020-07-17 15:54:53 +0100

    clicking "receive chat" will now fetch and display the message in HTML

--- api/src/main/java/doodle/fullstack/controller/HelloController.java
+++ api/src/main/java/doodle/fullstack/controller/HelloController.java
@@ -3,6 +3,7 @@ package doodle.fullstack.controller;
 
 import doodle.fullstack.ChatStorage;
 import doodle.fullstack.persistance.InMemoryChatStorage;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class HelloController {
 
     ChatStorage chatStorage = new InMemoryChatStorage();
 
+    @CrossOrigin(origins = "http://localhost:63342")
     @RequestMapping("/")
     public String index() {
         return
--- fe/js/chat.js
+++ fe/js/chat.js
@@ -1,13 +1,32 @@
 var APIget = "http://localhost:8080";
+var APIpost = "http://localhost:8080/chat";
 
 function fetchMessage(){
     fetch(APIget)
-        .then(response => response.json())
-        .then(json => appendToChat(json.message))
+        .then(response => response.text())
+        .then(text => appendToChat(text))
 }
 
-function postMessage(){
+function postTextMessage(){
+    function postText(text){
+        fetch(APIpost, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(text),
+        })
+            .then(response => response.text())
+            .then(text => appendToChat(text))
+    }
 }
 
 function appendToChat(text){
+
+    chatMessage = '<li style="width:100%;">' +
+        '<div class="">' +
+        '<p>'+text+'</p>' +
+        '</div>' +
+        '</li>';
+    $("ul").append(chatMessage);
 }
